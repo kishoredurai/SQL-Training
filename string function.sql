0820b0fe63b6c71c7392b47602e36673f5139640
Kishore D <kishoredurai7@gmail.com>
2023-04-04 10:47:17 +0530

    string operations

--- string function.sql
+++ string function.sql
@@ -9,3 +9,24 @@ select CONCAT(ltrim(name),'Email: ',rtrim(email)) from StudentDetails;
 select replace(department,'I','C') from StudentDetails
 
 select replace('hieveryone','e','e');
+
+select SUBSTRING(email,-2,5)from StudentDetails
+
+select TRANSLATE('helloee','eow','nkw'); -- replace the character
+
+select TRANSLATE(email,'ma','ke') from StudentDetails
+
+select replace(department,'IT','CT') from StudentDetails
+
+select LOWER('HELLOO');
+
+select upper(email) from StudentDetails;
+
+select str(458.31298,8,5);
+
+select right('welcome',3);
+select left('welcome', 3);
+
+select * from StudentDetails;
+update StudentDetails set department = (replace(department, 'IT' , 'CT'));
+
